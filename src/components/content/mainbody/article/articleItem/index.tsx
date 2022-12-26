f6847708c98e183e644950c6c05343b6c203d46e
fanshaojun <lyp82nlf>
2022-12-26 15:51:15 +0800

    fix: 布局调整

--- src/components/content/mainbody/article/articleItem/index.tsx
+++ src/components/content/mainbody/article/articleItem/index.tsx
@@ -6,37 +6,57 @@ type Props = {}
 
 // TODO: 单篇文章的Item
 const ArticleItem = (props: Props) => {
-    return (
-        <div className={style.item_container}>
-            {
-                TotalArticle.map((item: any, index: number) => {
+  return (
+    <div className={style.item_container}>
+      {
+        TotalArticle.map((item: any, index: number) => {
+          return (
+            <>
+              <div className={style.arcticle_item}>
+                <div className={style.arcticle_box}>
+                  <div className={style.item_top}>
+                    <span className={style.article_title}>{item.articleTitle}</span>
+                    <div className={style.content_container}>
+                      <span className={style.arcticle_content}>{limitWords(item.articleContent)}</span>
+                    </div>
+                  </div>
+                  <div className={style.item_bottom}>
+                    <span>{item.articleCreateTime}</span>
+                    <div className={style.bottom_tags}>{item.articleTags.map((_item: string, index: number) => {
+                      return (
+                        <span key={`${_item}_${index}`}>{_item}</span>
+                      )
+                    })}</div>
+                  </div>
+                </div>
+
+                <img src={item.articleCoverImg} alt="" />
+              </div>
+              {/* <div className={style.arcticle_item} key={`${item.articleTitle}_${index}`}>
+                <div className={style.item_top}>
+                  <span className={style.article_title}>{item.articleTitle}</span>
+                  <div className={style.content_container}>
+                    <span className={style.arcticle_content}>{limitWords(item.articleContent)}</span>
+                    <img src={item.articleCoverImg} alt="" />
+                  </div>
+                </div>
+                <div className={style.item_bottom}>
+                  <span>{item.articleCreateTime}</span>
+                  <div className={style.bottom_tags}>{item.articleTags.map((_item: string, index: number) => {
                     return (
-                        <>
-                            <div className={style.arcticle_item} key={`${item.articleTitle}_${index}`}>
-                                <div className={style.item_top}>
-                                    <span className={style.article_title}>{item.articleTitle}</span>
-                                    <div className={style.content_container}>
-                                        <span className={style.arcticle_content}>{limitWords(item.articleContent)}</span>
-                                        <img src={item.articleCoverImg} alt="" />
-                                    </div>
-                                </div>
-                                <div className={style.item_bottom}>
-                                    <span>{item.articleCreateTime}</span>
-                                    <div className={style.bottom_tags}>{item.articleTags.map((_item: string, index: number) => {
-                                        return (
-                                            <span key={`${_item}_${index}`}>{_item}</span>
-                                        )
-                                    })}</div>
-                                </div>
-                            </div>
-
-                        </>
+                      <span key={`${_item}_${index}`}>{_item}</span>
                     )
-                })
-            }
-        </div>
+                  })}</div>
+                </div>
+              </div> */}
 
-    )
+            </>
+          )
+        })
+      }
+    </div>
+
+  )
 }
 
 export default ArticleItem
